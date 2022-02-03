5e0f154145f92aec899d8db822ba352650ff3092
tyrelkostyk <tck290@mail.usask.ca>
2022-02-02 18:10:10 -0600

    cleaned up ex 2.2

--- lab2/tck290_lab2_ex2_2.py
+++ lab2/tck290_lab2_ex2_2.py
@@ -12,9 +12,6 @@ import socket
 import sys
 import os.path
 
-# create an IPv4 TCP socket
-serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
 # set address and port number of host server
 host, port = "localhost", 8080
 
@@ -24,91 +21,94 @@ bufferSize = 4096
 # set location of error file
 errorFile = "tck290_lab2_page_error.html"
 
-# attempt to bind to the socket
-try:
-	serverSocket.bind((host, port))
-except socket.error as msg:
-	print("Binding failed, Error code: " + str(msg[0]) + ", Message: " + msg[1])
-	sys.exit()
-print("Binding complete")
+# create an IPv4 TCP socket
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as serverSocket:
 
-# begin listening; allow up to 5 queued connections
-serverSocket.listen(5)
-print("Web Server hosting at " + str(host) + ":" + str(port))
+	# attempt to bind to the socket
+	try:
+		serverSocket.bind((host, port))
+	except socket.error as msg:
+		print("Binding failed, Error code: " + str(msg[0]) + ", Message: " + msg[1])
+		sys.exit()
+	print("Binding complete")
 
-while True:
-	# establish a TCP connection (blocking call)
-	clientSocket, addr = serverSocket.accept()
+	# begin listening; allow up to 5 queued connections
+	serverSocket.listen(5)
+	print("Web Server hosting at " + str(host) + ":" + str(port))
 
-	with clientSocket:
-		print("Connection established at ", addr)
+	while True:
+		# establish a TCP connection (blocking call)
+		clientSocket, addr = serverSocket.accept()
 
-		# receive data from client
-		data_in = clientSocket.recv(bufferSize)
+		with clientSocket:
+			print("Connection established at ", addr)
 
-		# exit on error
-		if not data_in:
-			print("Server: Error on recv")
-			break
+			# receive data from client
+			data_in = clientSocket.recv(bufferSize)
 
-		# extract request type and filename from HTTP request
-		req = data_in.decode("utf-8")
-		fields = req.split("\r\n")
-		request, file, version = fields[0].split(' ')
-		# remove leading slash
-		file = file[1:]
-
-		print(request + " request received for file: " + file + " with HTTP Version " + version)
-
-		# TODO: ensure file exists
-		if (os.path.isfile(file)):
-			# construct header
-			resp = version + " 200 OK\r\n\r\n"
-			# send header
-			error = clientSocket.sendall(resp.encode("utf-8"))
-
-			if error is not None:
-				print("Server: Error on sending 200 OK")
+			# exit on error
+			if not data_in:
+				print("Server: Error on recv")
 				break
 
-			# open and send file
-			with open(file, 'rb') as f:
-				error = clientSocket.sendall(f.read(bufferSize))
+			# extract request type and filename from HTTP request
+			req = data_in.decode("utf-8")
+			fields = req.split("\r\n")
+			request, file, version = fields[0].split(' ')
+			# remove leading slash
+			file = file[1:]
+
+			print(request + " request received for file: " + file + " with HTTP Version " + version)
+
+			# TODO: ensure file exists
+			if (os.path.isfile(file)):
+				# construct header
+				resp = version + " 200 OK\r\n\r\n"
+				# send header
+				error = clientSocket.sendall(resp.encode("utf-8"))
 
 				if error is not None:
-					print("Server: Error on sending page")
+					print("Server: Error on sending 200 OK")
 					break
 
-				endOfFile = "\r\n"
-				error = clientSocket.sendall(endOfFile.encode("utf-8"))
+				# open and send file
+				with open(file, 'rb') as f:
+					error = clientSocket.sendall(f.read(bufferSize))
+
+					if error is not None:
+						print("Server: Error on sending page")
+						break
+
+					endOfFile = "\r\n"
+					error = clientSocket.sendall(endOfFile.encode("utf-8"))
+
+					if error is not None:
+						print("Server: Error on sending end of page")
+						break
+
+			else:
+				# construct header
+				resp = version + " 404 Not Found\r\n\r\n"
+				# send header
+				error = clientSocket.sendall(resp.encode("utf-8"))
 
 				if error is not None:
-					print("Server: Error on sending end of page")
+					print("Server: Error on sending 404 Not Found")
 					break
 
-		else:
-			# construct header
-			resp = version + " 404 Not Found\r\n\r\n"
-			# send header
-			error = clientSocket.sendall(resp.encode("utf-8"))
+				# open and send file
+				with open(errorFile, 'rb') as f:
+					error = clientSocket.sendall(f.read(bufferSize))
 
-			if error is not None:
-				print("Server: Error on sending 404 Not Found")
-				break
+					if error is not None:
+						print("Server: Error on sending error page")
+						break
 
-			# open and send file
-			with open(errorFile, 'rb') as f:
-				error = clientSocket.sendall(f.read(bufferSize))
+					endOfFile = "\r\n"
+					error = clientSocket.sendall(endOfFile.encode("utf-8"))
 
-				if error is not None:
-					print("Server: Error on sending error page")
-					break
+					if error is not None:
+						print("Server: Error on sending end of page")
+						break
 
-				endOfFile = "\r\n"
-				error = clientSocket.sendall(endOfFile.encode("utf-8"))
-
-				if error is not None:
-					print("Server: Error on sending end of page")
-					break
-
-	print("Connection closed")
+		print("Connection closed")
